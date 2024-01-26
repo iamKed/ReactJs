560bcc8123e2e0fe93eab6db6fd1de8158cf26a2
iamKed <kedarswami9900@gmail.com>
2024-01-26 17:39:55 +0530

    third

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import React, { useRef,useState } from "react"
-import { Container} from "react-bootstrap";
-import { Form, Button,Card,CardBody } from "react-bootstrap";
+import React, { useRef, useState } from "react"
+import { Container } from "react-bootstrap";
+import { Form, Button, Card, CardBody } from "react-bootstrap";
 import { addDoc, collection, getDocs } from 'firebase/firestore/lite';
 import db from "../firebase";
 
@@ -23,8 +23,8 @@ function Main() {
     try {
       let data = {
         Name: userRef.current.value,
-        Email:userRef1.current.value,
-        Address:userRef2.current.value,
+        Email: userRef1.current.value,
+        Address: userRef2.current.value,
       }
       await addDoc(userCollectionRef, data);
       showUsers()
@@ -38,15 +38,22 @@ function Main() {
   return (
     <Container>
       <Form style={{
-        width: 500,
-        margin: "auto",
-        marginTop: 50,
+        width: '50%',
+        margin:'auto',
+        marginTop:50,
+        // marginBottom:50,
+        // marginLeft:400,
+        justifyContent:"center",
         border: "2px solid black",
-        padding: "40px"
-      }} onSubmit={updateData}>
+        padding: "40px",
+        borderRadius: 20,
+      }} >
+        
         <h2 style={{
-          marginBottom: 40
-        }}> FireBase Application</h2>
+          marginBottom: 40,
+          
+        }} className="display-4"> FireBase Application</h2>
+        
         <Form.Group controlId="form.Name">
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" ref={userRef} placeholder="Name" />
@@ -59,38 +66,35 @@ function Main() {
           <Form.Label>Address</Form.Label>
           <Form.Control type="text" ref={userRef2} placeholder="Address" />
         </Form.Group>
-        
-        <Button type="submit" style={{
+
+        <button style={{
           marginTop: 20
-        
-        }}>Submit</Button>
-        <Button style={{
+        }} type="button" className="btn btn-outline-success" onClick={updateData}>Submit</button>
+        <button style={{
           marginLeft: 20,
-          marginTop:20
-        }} onClick={showUsers}>Show Data</Button>
+          marginTop: 20
+        }} type="button" onClick={showUsers} className="btn btn-outline-dark">Show Data</button>
       </Form>
+      <hr style={{margin:50}}></hr>
       {users.map((user) => {
         return (
           <div>
             <Card>
               <CardBody>
-                <h5>First Name:{user.Name}</h5>
-                <h5>Last Name:{user.Email}</h5>
-                <h5>Last Name:{user.Address}</h5>
+                <h5>First Name: {user.Name}</h5>
+                <h5>Email: {user.Email}</h5>
+                <h5>Address: {user.Address}</h5>
               </CardBody>
             </Card>
           </div>
         );
       })}
-      
-      
-      
-      
+
+
+
+
     </Container>
   );
 }
 
-
-
 export default Main;
-
